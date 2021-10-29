a9c1de1e1a5536087d58fbd4c5f451600a9efe8e
suyog <satishbirhade16@gmail.com>
2021-10-29 10:40:04 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor()
+{
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
+    document.getElementById("Color").innerHTML=bgColor.toString()+";";   
+}
