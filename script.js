5b6418bdbf5a038973711dad9ef078e6cd772643
SatishB15 <satishbirhade16@gmail.com>
2024-04-06 19:40:13 +0530

    improved UI

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function changeColor()
-{
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    document.body.style.background = bgColor;
-    document.getElementById("Color").innerHTML=bgColor.toString()+";";   
+/**
+ * @description Function is used to generate the random rgb
+ */
+function changeColor() {
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const bgColor ="RGB ( "+ x + ", " + y + ", " + z + " )";
+    document.body.style.background = bgColor.toLowerCase().replace(" ","");
+    document.getElementById("rgb-color").innerHTML = bgColor.toString();
+}
+
+function copyColor() {
+    const colorValue = document.getElementById("rgb-color").innerHTML;
+    navigator.clipboard.writeText(colorValue)
 }
